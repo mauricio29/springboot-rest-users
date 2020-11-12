c019bb458bf923f48911cbee8a5752e0d30ecf4e
Mauricio I. D. <mauricio.293@gmail.com>
2020-11-12 16:31:52 -0300

    Springboot template project files
    Including:
    - spring-boot-starter-web
    - spring-boot-devtools
    - spring-boot-starter-test
    - lombok

---
+++ src/test/java/com/example/app_rest_users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_rest_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
