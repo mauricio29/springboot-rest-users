ca9ea406ffeda7c66506bb4b38404583b8e92b8e
Mauricio I. D. <mauricio.293@gmail.com>
2020-11-12 17:36:58 -0300

    New entity: User

---
+++ src/main/java/com/example/app_rest_users/entities/User.java
@@ -0,0 +1,22 @@
+package com.example.app_rest_users.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+	private String email;
+	private String password;
+	private String phone; // TODO: object
+
+}
