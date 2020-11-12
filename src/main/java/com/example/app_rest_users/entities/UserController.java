be4a96311a26622a19f66fa2b3262875a2277119
Mauricio I. D. <mauricio.293@gmail.com>
2020-11-12 20:43:41 -0300

    User REST - GET method find by id

--- src/main/java/com/example/app_rest_users/entities/UserController.java
+++ src/main/java/com/example/app_rest_users/entities/UserController.java
@@ -3,7 +3,9 @@ package com.example.app_rest_users.entities;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,16 @@ public class UserController {
 	@Autowired
 	private UserRepository repo;
 
+	/** GET - List all Users. */
 	@GetMapping("/users")
 	List<User> findAll() {
 		return repo.findAll();
+	}
+
+	/** GET	- Find user with given id. */
+	@GetMapping("/users/{id}")
+	User findOne(@PathVariable Long id) {
+		return repo.findById(id).get();
 	}
 
 }
